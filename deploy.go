9c43032e3c0c632304eb625fd4fede3799372aaf
softleader50\admin <ymhung0110@gmail.com>
2018-09-07 15:26:23 +0800

    TestStop

--- deploy.go
+++ deploy.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
 	"time"
 )
 
@@ -23,22 +25,59 @@ func (cmd *deployCmd) run() error {
 	d := t.Sub(time.Now())
 	c := time.After(d)
 	<-c
-	return deploy()
+	return deploy(cmd)
 }
 
-func deploy() error {
-
+func deploy(cmd *deployCmd) error {
 	// call 1~5
 
-	return nil
-}
-
-func stop() error {
 	// 1. Shutdown Tomcat
+	if err := stop(cmd.tomcatPath); err != nil {
+		fmt.Printf("stop error")
+		return err
+	}
+
+	// 2. 刪除 .../tomcat/webapps 目錄下的 payment.war 及 payment 資料夾
+	if err := deleteWebapp(cmd.tomcatPath + "\\webapps"); err != nil {
+		fmt.Printf("deleteWebapp error")
+		return err
+	}
+
+	// 3. 將 .../oldWar/payment.war 複製到 .../tomcat/webapps/
+	if err := backupWar(); err != nil {
+		fmt.Printf("backupWar error")
+		return err
+	}
+
+	// 4. 執行backupDb.bat 備份DB資料
+	if err := backupDb(); err != nil {
+		fmt.Printf("backupDb error")
+		return err
+	}
+
+	// 5. Startup Tomcat
+	if err := start(); err != nil {
+		fmt.Printf("start error")
+		return err
+	}
+}
+
+func stop(absFilePath string) error {
+	// 1. Shutdown Tomcat
+
+	fmt.Println("執行Shutdown path[" + absFilePath + "]")
+	//cmd := exec.Command("cmd.exe", "/C", "echo e")
+	//cmd := exec.Command("cmd.exe", "/C", "C:/D/act.txt")
+	cmd := exec.Command("cmd.exe", "/C", absFilePath)
+	d, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	fmt.Println("這是甚麼？" + string(d))
 	return nil
 }
 
-func deleteWebapp() error {
+func deleteWebapp(absFilePath string) error {
 	// 2. 刪除 .../tomcat/webapps 目錄下的 payment.war 及 payment 資料夾
 	return nil
 }
--- deploy_test.go
+++ deploy_test.go
@@ -1,43 +1,92 @@
 package main
 
 import (
-	"testing"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
+	"testing"
 )
 
+func TestCode(t *testing.T) {
+	fmt.Println("C:\\Users\\admin\\Desktop\\i519")
+	fmt.Println(strings.Replace("C:\\Users\\admin\\Desktop\\i519", "\\", "_", -1))
+
+}
+
 func TestStop(t *testing.T) {
-	err := stop()
+
+	// 產生臨時的檔案夾
+	tmp, err := ioutil.TempDir(os.TempDir(), "testDeploy")
+	//tmp, err := ioutil.TempDir("C:\\Users\\admin\\Desktop\\i519", "coppy")
 	if err != nil {
 		t.Error(err)
 	}
+	fmt.Printf("tmp = %v\n", tmp)
+
+	// 完成測試後刪除檔案夾
+	//defer os.RemoveAll(tmp)
+
+	// 在臨時檔案夾中, 建立 testCoppy.bat 檔案。
+	// bat內容：將自己(testCoppy.bat)另存新檔案為testCoppy_時分秒.bat
+	batPath := path.Join(tmp, "testCoppy.bat")
+	content := "@ECHO OFF\n"
+	content += ("FOR /F \"tokens=1-4 delims=:.\" %%a IN (\"%time%\") DO (\n")
+	content += ("set a=%%a\n")
+	content += ("set b=%%b\n")
+	content += ("set c=%%c\n")
+	content += (")\n")
+	content += ("CD " + tmp + "\n")
+	content += ("ping 127.0.0.1 -n 10 -w 1000\n")
+	content += ("COPY  testCoppy.bat " + tmp + "\\testCoppy_%a%%b%%c%.bat\n")
+	content += ("ping 127.0.0.1 -n 60 -w 1000\n")
+	content += ("ECHO finish")
+	ioutil.WriteFile(batPath, []byte(content), os.ModePerm)
+	fmt.Printf("batPath = %v\n", batPath)
+
+	// 驗證
+	err = stop(batPath)
+	//err = stop("C:\\Users\\admin\\Desktop\\i519\\testCoppy1.bat")
+	if err != nil {
+		fmt.Printf("有錯！")
+		t.Error(err)
+	}
+	err = stop(batPath)
+	//err = stop("C:\\Users\\admin\\Desktop\\i519\\testCoppy1.bat")
+	if err != nil {
+		fmt.Printf("有錯！")
+		t.Error(err)
+	}
+
 }
 
 func TestBackupWar(t *testing.T) {
-	// 產生臨時的檔案夾
-	tmp, err := ioutil.TempDir(os.TempDir(), "backup-war")
-	if err != nil {
-		t.Error(err)
-	}
-	// 完成測試後刪除檔案夾
-	defer os.RemoveAll(tmp)
+	/*
+		// 產生臨時的檔案夾
+		tmp, err := ioutil.TempDir(os.TempDir(), "backup-war")
+		if err != nil {
+			t.Error(err)
+		}
+		// 完成測試後刪除檔案夾
+		defer os.RemoveAll(tmp)
 
-	// 在臨時檔案夾中, 建立 myapp.war 檔案
-	warPath := path.Join(tmp, "myapp.war")
-	content := "hello"
-	ioutil.WriteFile(warPath, []byte(content), os.ModePerm)
+		// 在臨時檔案夾中, 建立 myapp.war 檔案
+		warPath := path.Join(tmp, "myapp.war")
+		content := "hello"
+		ioutil.WriteFile(warPath, []byte(content), os.ModePerm)
 
-	// 執行邏輯
-	backupWar()
+		// 執行邏輯
+		backupWar()
 
-	// 驗證
-	expected := path.Join(tmp, "backup", "myapp.war")
-	read, err := ioutil.ReadFile(expected)
-	if err != nil {
-		t.Error(err)
-	}
-	if actual := string(read); actual != content {
-		t.Errorf("expected content '%s', but got '%s'", content, actual)
-	}
+		// 驗證
+		expected := path.Join(tmp, "backup", "myapp.war")
+		read, err := ioutil.ReadFile(expected)
+		if err != nil {
+			t.Error(err)
+		}
+		if actual := string(read); actual != content {
+			t.Errorf("expected content '%s', but got '%s'", content, actual)
+		}
+	*/
 }
